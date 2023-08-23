5ea911180cb4394d605e61511397a35fdf8916fc
yogitanarvekar <narvekaryogita2001@gmail.com>
2023-08-23 13:34:54 +0530

    PrintNumEvenOrOdd

---
+++ src/javaCodes/PrintNumEvenOrOdd.java
@@ -0,0 +1,14 @@
+package javaCodes;
+
+public class PrintNumEvenOrOdd {
+    public static void main(String[] args) {
+        for (int i=1; i<=100; i++){
+            if (i % 2 == 0){
+                System.out.println(i+ " is even");
+            }
+            else {
+                System.out.println(i+ " is odd");
+            }
+        }
+    }
+}
