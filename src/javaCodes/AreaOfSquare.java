03641d5758cccee262f4886e8d9881cb32c3b043
yogitanarvekar <narvekaryogita2001@gmail.com>
2023-08-23 13:44:02 +0530

    Area of a square

---
+++ src/javaCodes/AreaOfSquare.java
@@ -0,0 +1,10 @@
+package javaCodes;
+
+public class AreaOfSquare {
+    public static void main(String[] args) {
+        double sidelength = 5;
+        double area = sidelength * sidelength;
+
+        System.out.println("The area of a square is: " +area);
+    }
+}
