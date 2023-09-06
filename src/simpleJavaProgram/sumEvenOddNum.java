faa883119e559b5b4ba2bbb97af6e2038aa15428
yogitanarvekar <narvekaryogita2001@gmail.com>
2023-09-06 19:01:04 +0530

    findNumDividedBy5

--- src/simpleJavaProgram/sumEvenOddNum.java
+++
@@ -1,6 +0,0 @@
-package simpleJavaProgram;
-
-public class sumEvenOddNum {
-    public static void main(String[] args) {
-
-
--- src/simpleJavaProgram/swapTwoNumber.java
+++ src/simpleJavaProgram/swapTwoNumber.java
@@ -2,13 +2,12 @@ package simpleJavaProgram;
 
 public class swapTwoNumber {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 12;
         int b = 10;
 
         System.out.println("Before swapping:");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-
 
         a = a + b;
         b = a - b;
