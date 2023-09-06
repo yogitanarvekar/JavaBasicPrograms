98c8ea2546de78496a5099580ccab47954e32a06
yogitanarvekar <narvekaryogita2001@gmail.com>
2023-09-06 22:22:17 +0530

    sumOfEvenOddNum

--- src/simpleJavaProgram/swapTwoNumber.java
+++ src/simpleJavaProgram/swapTwoNumber.java
@@ -2,16 +2,20 @@ package simpleJavaProgram;
 
 public class swapTwoNumber {
     public static void main(String[] args) {
-        int a = 12;
-        int b = 10;
+        int a = 5;
+        int b = 8;
+
 
         System.out.println("Before swapping:");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
 
-        a = a + b;
-        b = a - b;
-        a = a - b;
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+        int c = a;
+      a = b;
+      b = c;
 
         System.out.println("After swapping:");
         System.out.println("a = " + a);
