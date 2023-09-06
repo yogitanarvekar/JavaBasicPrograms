7d33dc7c2cce9f3f547376097d96928bf44b8ac8
yogitanarvekar <narvekaryogita2001@gmail.com>
2023-09-06 18:45:44 +0530

    swapTwoNumber

---
+++ src/simpleJavaProgram/swapTwoNumber.java
@@ -0,0 +1,21 @@
+package simpleJavaProgram;
+
+public class swapTwoNumber {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
