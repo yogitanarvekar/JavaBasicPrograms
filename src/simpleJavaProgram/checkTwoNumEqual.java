bb079474c666d866c4c080a49e7a51a46105c6d7
yogitanarvekar <narvekaryogita2001@gmail.com>
2023-09-06 19:09:43 +0530

    checkTwoNumEqual

---
+++ src/simpleJavaProgram/checkTwoNumEqual.java
@@ -0,0 +1,14 @@
+package simpleJavaProgram;
+
+public class checkTwoNumEqual {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 13;
+
+        if (num1 == num2) {
+            System.out.println("two numbers are equal");
+        } else {
+            System.out.println("two numbers are not equal");
+        }
+    }
+}
